7038d3c41f5e17f0540320bbfc256dcee568c257
Travis Czerw <travis.czerw@gmail.com>
2015-02-16 13:06:08 -0600

    HW 1 done. Button added

---
+++ scripts/controllers.js
@@ -0,0 +1,16 @@
+angular.module('app.controllers', [])
+.controller('buttonController', function($scope, $timeout) {
+
+	$scope.buttonText = "Submit";
+	$scope.isLoading = "Loading...";
+	$scope.disableButton = false;
+
+	$scope.onClick = function() {
+		$scope.disableButton = true;
+		$scope.buttonText = $scope.isLoading;
+		$timeout(function(){ 
+			$scope.disableButton = false
+			$scope.buttonText = "Submit" }, 2000);
+	}
+
+});
